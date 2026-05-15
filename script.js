e63251fa77520eb50f6c607933d6cdbab596af03
vkidia <kekannumber1@gmail.com>
2026-05-15 13:33:04 +0300

    2 и 3 фотка

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
 // ==============================
 
 const photos = [
-  { src: "photos/photo1.jpg", caption: "подпись к фото 1" },
-  { src: "photos/photo2.jpg", caption: "безумной" },
-  { src: "photos/photo3.jpg", caption: "подпись к фото 3" },
-  { src: "photos/photo4.jpg", caption: "подпись к фото 4" },
-  { src: "photos/photo5.jpg", caption: "подпись к фото 5" },
-  { src: "photos/photo6.jpg", caption: "подпись к фото 6" },
+  { src: "photo/photo1.jpg", caption: "подпись к фото 1" },
+  { src: "photo/photo2.jpg", caption: "безумной" },
+  { src: "photo/photo3.jpg", caption: "творческой" },
+  { src: "photo/photo4.jpg", caption: "подпись к фото 4" },
+  { src: "photo/photo5.jpg", caption: "подпись к фото 5" },
+  { src: "photo/photo6.jpg", caption: "подпись к фото 6" },
   // добавляй новые строчки сюда:
-  // { src: "photos/photo7.jpg", caption: "подпись" },
+  // { src: "photo/photo7.jpg", caption: "подпись" },
 ];
 
 const tracks = [
